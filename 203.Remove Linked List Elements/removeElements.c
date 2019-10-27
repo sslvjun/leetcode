f554a3b991d82f9a7ace67e8576f96001de31ec3
iSun <sslvjun@gmail.com>
2019-10-27 22:09:16 +0800

    Add solutions

---
+++ 203.Remove Linked List Elements/removeElements.c
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* removeElements(struct ListNode* head, int val){
+    if (head == NULL)
+        return NULL;
+    
+    head->next = removeElements(head->next, val);
+    if (head->val == val)
+        return head->next;
+    else
+        return head;
+}
---
+++ 234.Palindrome Linked List/isPalindrome.c
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+bool isPalindrome(struct ListNode* head){
+    struct ListNode *fast = head, *slow = head, *prev = NULL;
+    
+    while (fast) {  // find mid node
+        slow = slow->next;
+        fast = fast->next ? fast->next->next : fast->next;
+    }
+    
+    while (slow) {  // reverse
+        struct ListNode *tmp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = tmp;
+    }
+    
+    while (head && prev) {  // check
+        if (head->val != prev->val)
+            return false;
+        
+        head = head->next;
+        prev = prev->next;
+    }
+    
+    return true;
+}
