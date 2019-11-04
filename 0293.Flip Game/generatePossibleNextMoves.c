f34a16da82c13f7502082b9067f038c413c785c5
iSun <sslvjun@gmail.com>
2019-11-04 13:20:59 +0800

    Add solutions

---
+++ 0293.Flip Game/generatePossibleNextMoves.c
@@ -0,0 +1,32 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char ** generatePossibleNextMoves(char * s, int* returnSize){
+    int len = strlen(s);
+    int i, cnt = 0;
+    
+    char **result = (char **)malloc(len * sizeof(char *));
+    if (result == NULL) {
+        return NULL;
+    }
+    
+    memset(result, 0, len * sizeof(char *));
+    
+    for (i=0; i<len -1; i++) {
+        if (s[i] == '+' && s[i + 1] == '+') {
+            result[cnt] = (char *) malloc(len + 1);
+            if (result[cnt] == NULL) {
+                continue;
+            }
+            
+            memset(result[cnt], 0, len + 1);
+            strcpy(result[cnt], s);
+            result[cnt][i] = '-';
+            result[cnt][i + 1] = '-';
+            cnt++;
+        }
+    }
+    
+    *returnSize = cnt;
+    return result;
+}
